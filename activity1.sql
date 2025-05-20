3355f3388ee623d50cf7f7c506f62e3b383a4076
Harrison2607 <leena.harrison26@gmail.com>
2025-05-20 19:58:41 +0530

    SQL

---
+++ activity1.sql
@@ -0,0 +1,27 @@
+
+CREATE TABLE harrison (
+
+SNO TEXT PRIMARY KEY,
+
+SNAME TEXT,
+
+STATUS INTEGER,
+
+CITY TEXT
+
+);
+
+INSERT INTO harrison (SNO, SNAME, STATUS, CITY) VALUES
+
+('S1', 'Smith', 20, 'London'),
+
+('S2', 'Jones', 10, 'Paris'),
+
+('S3', 'Blake', 30, 'Paris'),
+
+('S4', 'Clarke', 20, 'London'),
+
+('S5', 'Adams', 30, 'Athens');
+
+SELECT * FROM harrison;
+
